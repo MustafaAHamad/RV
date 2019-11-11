6b317414ee490208dc3b4770ddf6db3f74b3d170
MustafaAHamad <34164529+MustafaAHamad@users.noreply.github.com>
2019-11-11 10:00:02 -0500

    v2 stuff

---
+++ version 2/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<html lang="eng">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="client/css/style.css?v=<?php echo time();?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<title>RV - Login or Signup</title>
+</head>
+<body>
+	<!-- CONTENT CONTAINER -->
+	<div id="vkR93ckBu">
+		<div class="iC5oVne1l">
+
+			<div class="vjR8cBnqiy">
+				<div class="cPbM42jGh">
+					<div class="ghR3kjMt">
+						<a class="yUtEc46bQ">
+							<img alt="" src="client/img/whodis.png" />
+							<span class="vBnr39CkriP">Kamado Tanjirō</span>
+							<span class="vBnr39CkriP vkFrj48Cxy">6m</span>
+						</a>
+					</div>
+					<div class="NdtU7Bpa">
+						<p>SKRRRT BrRRT sKRt SKKRT</p>
+						<div class="GTrjcR941">
+							<img class="vkrhjEi8p" src="client/img/banner.jpg">
+						</div>
+					</div>
+					<!-- POST ACTIONS -->
+					<div class="qC0liByGe"></div>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</body>
+</html>
